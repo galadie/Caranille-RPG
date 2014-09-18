e8730d9898ea8c2073eb81ea2abfa5a6fb621481
galadie <galadie@users.noreply.github.com>
2014-09-18 11:15:16 +0200

    Update index.php
    gestion du bannissement d'IP

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 // chargement 
 require_once("Kernel/Load.php"); 
 
+if (is_ban(getRealIpAddr())) 
+{
+    header('HTTP/1.0 403 Forbidden');
+     //L'IP est bannie, on affiche la page et on arrête le script
+     readfile($_path.'Ban.php');
+     die();
+}
+
 // recuperation de la command
 list($page,$secteur_module) = get_module_and_page() ;
 
